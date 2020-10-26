2411c36b80972ae89b1fe0c4b8129ec1f5499f6c
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-10-26 19:28:32 -0300

    Lidando com exceções Java

---
+++ src/MinhaExcecao.java
@@ -0,0 +1,10 @@
+
+public class MinhaExcecao extends RuntimeException {
+	public MinhaExcecao(String msg) {
+		super(msg);
+	}
+}
+
+//Olhando a classe RuntimeException, notamos que ela possui o construtor 
+//que recebe uma message. Ele chama o super e manda a mensagem para a classe mãe. 
+//É uma boa prática para não perder a mensagem.
