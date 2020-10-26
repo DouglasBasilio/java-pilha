53b09587594acd60db4deda4e4c1269d8446f760
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-10-26 20:49:29 -0300

    Capturando qualquer exceção

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -24,7 +24,12 @@ public class FluxoComTratamento {
             System.out.println(i);
             //int a = i / 0;
             Conta c = null;
-            c.deposita();
+            try {
+				c.deposita();
+			} catch (MinhaExcecao e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
         System.out.println("Fim do metodo2");
     }
